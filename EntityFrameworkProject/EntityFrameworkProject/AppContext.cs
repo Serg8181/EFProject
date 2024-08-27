35cd4384900b30e452349fdf154d81f590241a3f
Serg8181 <147308625+Serg8181@users.noreply.github.com>
2024-08-27 11:31:33 +0300

    Add project

---
+++ EntityFrameworkProject/EntityFrameworkProject/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject
+{
+    internal class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-38FCAKT\SQLEXPRESS;Database=EF;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ EntityFrameworkProject/EntityFrameworkProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using EntityFrameworkProject;
+
+static void Main(string[] args)
+{
+    using (var db = new EntityFrameworkProject.AppContext())
+    {
+        var user1 = new User { Name = "Arthur", Role = "Admin" };
+        var user2 = new User { Name = "Klim", Role = "User" };
+
+        db.Users.Add(user1);
+        db.Users.Add(user2);
+        db.SaveChanges();
+        Console.ReadKey();
+    }
+}
